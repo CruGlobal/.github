bebb29de586360d849cee605d8d77b46296a858a
Brian Zoetewey <brian.zoetewey@cru.org>
2022-03-04 15:40:12 -0500

    Secrets aren't always secret.

--- src/secrets.js
+++ src/secrets.js
@@ -1,17 +1,20 @@
 import * as core from '@actions/core'
-import {buildSecrets} from './ecs-config'
+import { buildSecrets } from './ecs-config'
 
 async function run () {
   try {
-    const projectName = core.getInput('project-name', {required: false}) || process.env.PROJECT_NAME
-    const environment = core.getInput('environment', {required: false}) || process.env.ENVIRONMENT
+    const projectName = core.getInput('project-name', { required: false }) || process.env.PROJECT_NAME
+    const environment = core.getInput('environment', { required: false }) || process.env.ENVIRONMENT
 
     core.debug(`Project Name: ${projectName}`)
     core.debug(`Environment: ${environment}`)
 
     const secrets = await buildSecrets(projectName, environment)
     for (const key in secrets) {
-      core.setSecret(secrets[key])
+      // Only mark the value of a secret as secret if it doesn't match projectName or environment
+      if (![projectName, environment].includes(secrets[key])) {
+        core.setSecret(secrets[key])
+      }
       core.exportVariable(key, secrets[key])
     }
 
@@ -21,4 +24,3 @@ async function run () {
 }
 
 run()
-
