bf77f798e8835bca47715b77f41a3d378659a0d5
Brian Zoetewey <brian.zoetewey@cru.org>
2022-03-25 11:26:42 -0400

    Add error handling. Checkout serverless project to a folder.

--- src/build-number.js
+++ src/build-number.js
@@ -2,11 +2,15 @@ import * as core from '@actions/core'
 import { ecsBuildNumber } from './aws'
 
 async function run () {
-  const projectName = core.getInput('project-name', { required: false }) || process.env.PROJECT_NAME
-  const buildNumber = parseInt(await ecsBuildNumber(projectName))
+  try {
+    const projectName = core.getInput('project-name', { required: false }) || process.env.PROJECT_NAME
+    const buildNumber = parseInt(await ecsBuildNumber(projectName))
 
-  core.setOutput('build-number', buildNumber)
-  core.exportVariable('BUILD_NUMBER', buildNumber)
+    core.setOutput('build-number', buildNumber)
+    core.exportVariable('BUILD_NUMBER', buildNumber)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
 
 run()
