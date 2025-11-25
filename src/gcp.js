d849aadff5d6e25023b10a09de9627e698172955
Brian Zoetewey <brian.zoetewey@cru.org>
2025-11-25 13:25:51 -0500

    Fix secret version name

--- src/gcp.js
+++ src/gcp.js
@@ -9,7 +9,7 @@ export async function secrets (project, types = PARAM_TYPES) {
     })
 
     return await secrets.reduce(async (acc, secret) => {
-        const [version] = await client.accessSecretVersion({name: `${secret.name}/latest`})
+        const [version] = await client.accessSecretVersion({name: `${secret.name}/versions/latest`})
         return { ...acc, [secret.name.split('/').pop()]: version.payload.data.toString() }
     }, Promise.resolve({}))
 }
